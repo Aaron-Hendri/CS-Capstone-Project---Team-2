ebc20bfcdef916582ca1698f76db6acfd07f2b49
Gabriel Ruiz <65831149+TrillGabey@users.noreply.github.com>
2023-03-28 14:14:08 -0500

    Add files via upload
    Signed-off-by: Gabriel Ruiz <65831149+TrillGabey@users.noreply.github.com>

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+// Connect to the database
+require_once "config.php";
+//session_start();
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+if($conn->connect_error){
+  die("Connection Failed: ".$conn->connect_error);
+}
+echo "Connected successfully".'<br>';
+
+if(isset($_POST['Login'])) {
+  //if($_POST['userType'] == 'faculty')
+  $myusername = $_POST['username'];
+  $mypassword = $_POST['password'];
+
+  $sql = "SELECT * FROM login WHERE username = '$myusername' and password = '$mypassword'";
+  //$sql = "select * from login";
+  $result = $conn->query($sql);
+  $row = mysqli_fetch_assoc($result);
+  
+  echo  'My username is '.$myusername.'<br>';
+  echo  'My password is '.$mypassword.'<br>';
+  echo $row.'<br>';
+
+  if(mysqli_num_rows($result) == 1){
+  //if($myusername = $row['username'] && $mypassword = $row['password']){
+    header("location: MainPage.html");
+  }
+  else {
+    $error = "Your Login Name or Password is invalid013";
+    echo $error;
+ }
+}
+// Close the database connection and statement
+mysqli_close($result);
+mysqli_close($conn);
+?>
